6f41b2c6a43724510ecab58b6f82328b022741cf
Shawn Kang <shineksu813@outlook.com>
2024-10-31 21:28:49 +0900

    009 Implementing ChatDetailView

--- frontend/app/src/main/java/com/imeanttobe/drawapplication/view/chat/ChatDetailView.kt
+++ frontend/app/src/main/java/com/imeanttobe/drawapplication/view/chat/ChatDetailView.kt
@@ -1,30 +1,46 @@
 package com.imeanttobe.drawapplication.view.chat
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.automirrored.rounded.Send
 import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.imeanttobe.drawapplication.viewmodel.ChatDetailViewModel
@@ -38,16 +54,23 @@ fun ChatDetailView(
         modifier = modifier,
         topBar = { ChatDetailViewTopBar(
             opponentNickname = viewModel.opponentNickname.value
+        ) },
+        bottomBar = { ChatDetailViewBottomBar(
+            text = viewModel.message.value,
+            onValueChange = viewModel::setMessage,
+            onSendClick = viewModel::send
         ) }
     ) { innerPadding ->
         Box(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding),
+                .padding(innerPadding)
+                .fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            LazyColumn {
-                items(10) {
+            LazyColumn(
+                verticalArrangement = Arrangement.Top
+            ) {
+                items(30) {
                     ChatBubble(
                         message = "message",
                         isMine = it % 2 == 0
@@ -101,14 +124,14 @@ fun ChatBubble(
     ) {
         Box(
             modifier = Modifier
-                .clip(RoundedCornerShape(100.dp))
+                .clip(RoundedCornerShape(10.dp))
                 .background(color = if (isMine) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondary)
                 .padding(10.dp),
             contentAlignment = Alignment.CenterStart,
         ) {
             Text(
                 text = message,
-                style = MaterialTheme.typography.bodyLarge.copy(
+                style = MaterialTheme.typography.bodyMedium.copy(
                     color = if (isMine) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onSecondary
                 )
             )
@@ -117,27 +140,105 @@ fun ChatBubble(
 }
 
 @Composable
-fun ChatTextFieldContainer(
+fun ChatDetailViewBottomBar(
     text: String,
     onValueChange: (String) -> Unit,
     onSendClick: () -> Unit
 ) {
-    Column(
+    BottomAppBar(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(5.dp)
+            .height(72.dp),
     ) {
-        ChatTextField()
-        ChatSendButton()
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(5.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            ChatTextField(
+                text = text,
+                onValueChange = onValueChange
+            )
+            ChatSendButton(
+                onSendClick = onSendClick
+            )
+        }
     }
 }
 
 @Composable
-fun ChatTextField() {
-
+fun ChatTextField(
+    text: String,
+    onValueChange: (String) -> Unit
+) {
+    BasicTextField(
+        textStyle = MaterialTheme.typography.bodyLarge.copy(color = MaterialTheme.colorScheme.onBackground),
+        value = text,
+        onValueChange = onValueChange,
+        singleLine = true,
+        maxLines = 1,
+        cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
+        decorationBox = @Composable { innerTextField ->
+            Box(
+                modifier = Modifier
+                    .size(width = 320.dp, height = 30.dp)
+                    .clip(RoundedCornerShape(100.dp))
+                    .border(
+                        border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
+                        shape = RoundedCornerShape(100.dp)
+                    )
+                    .padding(vertical = 5.dp, horizontal = 10.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                innerTextField()
+            }
+        }
+    )
+    /*
+    @Composable { innerTextField ->
+                    OutlinedTextFieldDefaults.DecorationBox(
+                        value = value,
+                        visualTransformation = visualTransformation,
+                        innerTextField = innerTextField,
+                        placeholder = placeholder,
+                        label = label,
+                        leadingIcon = leadingIcon,
+                        trailingIcon = trailingIcon,
+                        prefix = prefix,
+                        suffix = suffix,
+                        supportingText = supportingText,
+                        singleLine = singleLine,
+                        enabled = enabled,
+                        isError = isError,
+                        interactionSource = interactionSource,
+                        colors = colors,
+                        container = {
+                            OutlinedTextFieldDefaults.Container(
+                                enabled = enabled,
+                                isError = isError,
+                                interactionSource = interactionSource,
+                                colors = colors,
+                                shape = shape,
+                            )
+                        }
+                    )
+                }
+     */
 }
 
 @Composable
-fun ChatSendButton() {
-
+fun ChatSendButton(
+    onSendClick: () -> Unit
+) {
+    IconButton(
+        modifier = Modifier.size(32.dp),
+        onClick = onSendClick
+    ) {
+        Icon(
+            imageVector = Icons.AutoMirrored.Rounded.Send,
+            contentDescription = null
+        )
+    }
 }
--- frontend/app/src/main/java/com/imeanttobe/drawapplication/viewmodel/ChatDetailViewModel.kt
+++ frontend/app/src/main/java/com/imeanttobe/drawapplication/viewmodel/ChatDetailViewModel.kt
@@ -9,18 +9,23 @@ import javax.inject.Inject
 @HiltViewModel
 class ChatDetailViewModel @Inject constructor() : ViewModel() {
     // Values
-    private val _currentMessage = mutableStateOf("")
+    private val _message = mutableStateOf("")
     private val _opponentNickname = mutableStateOf("채팅 상대 닉네임")
 
     // Getter
-    val currentMessage: State<String> = _currentMessage
+    val message: State<String> = _message
     val opponentNickname: State<String> = _opponentNickname
 
     // Setter
-    fun setCurrentMessage(newValue: String) {
-        _currentMessage.value = newValue
+    fun setMessage(newValue: String) {
+        _message.value = newValue
     }
     fun setOpponentNickname(newValue: String) {
         _opponentNickname.value = newValue
+    }
+
+    // Methods
+    fun send() {
+
     }
 }
