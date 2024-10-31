2b1aeab0642e71f7b21a984a4e5e130316a3c2c3
wang seong min <163216266+cauwang@users.noreply.github.com>
2024-10-31 17:11:49 +0900

    006 Loginpage create UI text button

--- frontend/app/src/main/java/com/imeanttobe/drawapplication/view/welcome/LoginView.kt
+++ frontend/app/src/main/java/com/imeanttobe/drawapplication/view/welcome/LoginView.kt
@@ -1,16 +1,33 @@
 package com.imeanttobe.drawapplication.view.welcome
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.imeanttobe.drawapplication.R
 import com.imeanttobe.drawapplication.viewmodel.LoginViewModel
+
 
 @Composable
 fun LoginView(
@@ -18,17 +35,24 @@ fun LoginView(
     viewModel: LoginViewModel = hiltViewModel()
 ) {
     Scaffold(modifier = modifier) { innerPadding ->
-        Box(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding),
-            contentAlignment = Alignment.Center
+            verticalArrangement = Arrangement.Center, // 수직 중앙 정렬
+            horizontalAlignment = Alignment.CenterHorizontally // 수평 중앙 정렬
         ) {
-            Text(text = viewModel.username.value)
-            TextField(
-                value = viewModel.username.value,
-                onValueChange = { newValue -> viewModel.setUsername(newValue) }
-            )
+            Text(text = "Draw",fontSize = 40.sp,               // 텍스트 크기
+                fontWeight = FontWeight.Bold)
+            Text(text = "창작가들을 위한 공간",fontSize = 15.sp,)
+
+            Spacer(modifier = Modifier.height(16.dp)) // 16dp의 간격 추가
+
+            ElevatedButton(modifier = Modifier.width(257.dp).height(37.dp),
+
+                    colors = ButtonDefaults.elevatedButtonColors(containerColor = Color(0xFF0073FF),
+                        contentColor =Color.White)
+                ,onClick = {}){ Text("로그인")}
+            }
         }
     }
-}
