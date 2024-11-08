5465cb9976e6c2111f0c5fdf0fa2180ddc84ff12
wang seong min <163216266+cauwang@users.noreply.github.com>
2024-11-08 15:10:53 +0900

    015 Solving conflict and merge
    기존 longindetail 만들어놓은거 충돌돼서 해결했습니다

--- frontend/app/src/main/java/com/imeanttobe/drawapplication/view/welcome/LoginView.kt
+++ frontend/app/src/main/java/com/imeanttobe/drawapplication/view/welcome/LoginView.kt
@@ -1,24 +1,36 @@
 package com.imeanttobe.drawapplication.view.welcome
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.imeanttobe.drawapplication.R
 import com.imeanttobe.drawapplication.viewmodel.LoginViewModel
 
 
@@ -66,15 +78,42 @@ fun LogindetailView(
             verticalArrangement = Arrangement.Center, // 수직 중앙 정렬
             horizontalAlignment = Alignment.CenterHorizontally // 수평 중앙 정렬
         ) {
-            Text(text = "loginpage",fontSize = 40.sp,               // 텍스트 크기
-                fontWeight = FontWeight.Bold)
-            Text(text = "창작가들을 위한 공간",fontSize = 15.sp,)
-
+            Text(
+                modifier = Modifier.padding(horizontal = 30.dp).align(alignment = Alignment.Start),
+                text = "DRAW",
+                fontSize = 40.sp,               // 텍스트 크기
+                fontWeight = FontWeight.Bold
+            )
             Spacer(modifier = Modifier.height(16.dp)) // 16dp의 간격 추가
+
+
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Enter your ID") },
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 30.dp),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.Person, // 원하는 아이콘으로 변경
+                        contentDescription = "사용자 아이콘" // 접근성을 위한 설명
+                    )
+                }
+            )
+
+            Spacer(modifier = Modifier.height(8.dp)) // 16dp의 간격 추가
+
+
+            TextField(modifier = Modifier.fillMaxWidth().padding(horizontal = 30.dp),
+                value = "",
+                onValueChange = {},
+                label = { Text(text = "Enter your Password") },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Filled.Lock, contentDescription = "비밀번호 아이콘")})
+
+            Spacer(modifier = Modifier.height(16.dp))
+            HorizontalDivider(modifier = Modifier.width(500.dp).padding(horizontal = 30.dp))
 
 
         }
     }
-
-
 }
