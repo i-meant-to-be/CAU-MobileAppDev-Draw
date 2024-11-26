a0ab3de8db9bdb3310ffaae5c9dc48fc8f073858
wang seong min <163216266+cauwang@users.noreply.github.com>
2024-11-26 14:59:13 +0900

    039 Implementing ProfileView

--- frontend/app/src/main/java/com/imeanttobe/drawapplication/view/bottomnav/ProfileView.kt
+++ frontend/app/src/main/java/com/imeanttobe/drawapplication/view/bottomnav/ProfileView.kt
@@ -25,13 +25,22 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -39,10 +48,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil3.compose.AsyncImage
+import com.google.firebase.auth.FirebaseAuth
 import com.imeanttobe.drawapplication.R
 import com.imeanttobe.drawapplication.viewmodel.ProfileViewModel
 
@@ -122,6 +133,8 @@ fun ProfileCard(
 ){
     val backgroundColor = MaterialTheme.colorScheme.primaryContainer
     val contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+    val currentUser = FirebaseAuth.getInstance().currentUser
+    var showDialog by remember { mutableStateOf(false) }
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.StartActivityForResult()
     ) { result ->
@@ -133,7 +146,7 @@ fun ProfileCard(
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 50.dp,horizontal = 10.dp)
+            .padding(vertical = 50.dp, horizontal = 10.dp)
     ) {
         Column(
             modifier = Modifier
@@ -156,38 +169,123 @@ fun ProfileCard(
             Text(text = "Drawing is my life",style = MaterialTheme.typography.bodySmall)
         }
 
-        Button(
-            onClick = { /*TODO*/ },
-            modifier = Modifier
-                .padding(10.dp)
-                .align(Alignment.TopEnd)
-                .width(58.dp)
-                .height(20.dp)
-                .clip(RoundedCornerShape(5.dp)),// 배경색 설정
-            shape = RoundedCornerShape(5.dp), // 버튼 모양 설정
-            contentPadding = PaddingValues(0.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = backgroundColor,
-                contentColor = contentColor
-            )
-        ) {
-            Text(text = stringResource(id=R.string.modify_information), fontSize = 10.sp)
-        }
+        if (true) {
+            Button(
+                onClick = {showDialog=true},
+                modifier = Modifier
+                    .padding(10.dp)
+                    .align(Alignment.TopEnd)
+                    .width(58.dp)
+                    .height(20.dp)
+                    .clip(RoundedCornerShape(5.dp)),// 배경색 설정
+                shape = RoundedCornerShape(5.dp), // 버튼 모양 설정
+                contentPadding = PaddingValues(0.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = backgroundColor,
+                    contentColor = contentColor
+                )            ) {
+                Text(text = stringResource(id = R.string.modify_information), fontSize = 10.sp)
+            }
+            if (showDialog) {
+                AlertDialog(
+                    containerColor = Color.White,
+                    onDismissRequest = { showDialog = false },
+                    title = { Box(modifier=Modifier.fillMaxWidth()
+                        , contentAlignment = Alignment.Center){
+                        Text("프로필 정보 수정",style = MaterialTheme.typography.labelLarge, fontSize = 18.sp)} },
+                    text = {
+                        Column() {
+                        Text(
+                            modifier = Modifier.padding(horizontal = 10.dp),
+                            text = "Nick Name",
+                            fontSize = 15.sp,               // 텍스트 크기
+                            fontWeight = FontWeight.Bold
+                        )
 
-        Button(
-            onClick = {
-                val intent = Intent(
-                    Intent.ACTION_PICK,
-                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                        OutlinedTextField(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 10.dp),
+                            value = "",
+                            onValueChange = {},
+                            placeholder = { Text(text = "닉네임을 입력하시오")},
+                            leadingIcon = {
+                                Icon(imageVector = Icons.Filled.Lock, contentDescription = "비밀번호 아이콘")
+                            })
+
+                            Spacer(Modifier.height(10.dp))
+                            Text(
+                                modifier = Modifier.padding(horizontal = 10.dp),
+                                text = "Role",
+                                fontSize = 15.sp,               // 텍스트 크기
+                                fontWeight = FontWeight.Bold
+                            )
+
+                            OutlinedTextField(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 10.dp),
+                                value = "",
+                                onValueChange = {},
+                                placeholder = { Text(text = "역할을 입력하시오")},
+                                leadingIcon = {
+                                    Icon(imageVector = Icons.Filled.Lock, contentDescription = "비밀번호 아이콘")
+                                })
+                            Spacer(Modifier.height(10.dp))
+
+                            Text(
+                                modifier = Modifier.padding(horizontal = 10.dp),
+                                text = "Introduce one sentence",
+                                fontSize = 15.sp,               // 텍스트 크기
+                                fontWeight = FontWeight.Bold
+                            )
+
+                            OutlinedTextField(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 10.dp),
+                                value = "",
+                                onValueChange = {},
+                                placeholder = { Text(text = "한 줄 소개")},
+                                leadingIcon = {
+                                    Icon(imageVector = Icons.Filled.Lock, contentDescription = "비밀번호 아이콘")
+                                })
+                        }
+
+                        // 정보 입력 폼 추가
+                        // 예: OutlinedTextField, Button 등
+                    },
+                    confirmButton = {
+                        Button(onClick = {
+                            // 정보 저장 로직 추가
+                            showDialog = false // dialog 닫기
+                        }) {
+                            Text("저장")
+                        }
+                    },
+                    dismissButton = {
+                        Button(onClick = { showDialog = false }) {
+                            Text("취소")
+                        }
+                    }
                 )
-                launcher.launch(intent)
-            },
-            modifier = Modifier
-                .padding(10.dp)
-                .align(Alignment.BottomEnd)
-                .width(58.dp)
-                .height(20.dp)
-                .clip(RoundedCornerShape(5.dp)),// 배경색 설정
+            }
+
+
+            Button(
+                onClick = {
+                    val intent = Intent(
+                        Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    )
+                    launcher.launch(intent)
+                },
+                modifier = Modifier
+                    .padding(10.dp)
+                    .align(Alignment.BottomEnd)
+                    .width(58.dp)
+                    .height(20.dp)
+                    .clip(RoundedCornerShape(5.dp)),// 배경색 설정
             shape = RoundedCornerShape(5.dp), // 버튼 모양 설정
             contentPadding = PaddingValues(0.dp),
             colors = ButtonDefaults.buttonColors(
@@ -198,4 +296,22 @@ fun ProfileCard(
             Text(text = stringResource(id = R.string.register_picture), fontSize = 10.sp)
         }
     }
+        else{
+            OutlinedButton(onClick = {},modifier = Modifier
+                    .padding(10.dp)
+                .align(Alignment.TopEnd)
+                .width(58.dp)
+                .height(20.dp)
+                .clip(RoundedCornerShape(5.dp)),// 배경색 설정
+                shape = RoundedCornerShape(5.dp), // 버튼 모양 설정
+                contentPadding = PaddingValues(0.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = backgroundColor,
+                    contentColor = contentColor
+                )            ) {
+                Text(text = stringResource(id = R.string.direct_message), fontSize = 10.sp)
+            }}
+
 }
+}
+
